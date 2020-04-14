3a5475387f2c840863dad3f61b5d7191bf066cc8
nwksg <583747580@qq.com>
2020-04-14 09:55:00 +0800

    作业6 24-niweikang finished post data

--- post_youdao.py
+++ post_youdao.py
@@ -1,6 +1,8 @@
 from random import random
 import random
 import requests
+import time
+import hashlib
 
 # url = "http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
 # content = " 我爱中国 "
@@ -15,27 +17,20 @@ class Youdao():
         self.sign = self.get_sign()
 
     def get_salt(self):
-        s = str(random.randint(0, 10))
-        t = self.ts
-        return t + s
+        return self.ts + str(random.randint(0, 10))
 
     def get_md5(self, value):
-        import hashlib
         m = hashlib.md5()
         m.update(value.encode("utf-8"))
         return m.hexdigest()
 
     def get_sign(self):
-        e = self.content
-        i = self.salt
-        s = "fanyideskweb" + e + i + "Nw(nmmbP%A-r6U3EUn]Aj"
+        s = "fanyideskweb" + self.content + self.salt + "Nw(nmmbP%A-r6U3EUn]Aj"
         return self.get_md5(s)
 
     def get_ts(self):
-        import time
         t = time.time()
-        ts = str(int(round(t * 1000)))
-        return ts
+        return str(int(round(t * 1000)))
 
     def yield_form_data(self):
         form_data = {
@@ -55,19 +50,22 @@ class Youdao():
         }
         return form_data
 
-    def get_headers(self):
-        headers = {
+    def yield_headers(self):
+        return {
             'Cookie': 'OUTFOX_SEARCH_USER_ID=-891979111@10.108.160.17; OUTFOX_SEARCH_USER_ID_NCOO=1761989640.4588385; _ntes_nnid=86bc47b67c933d9fce4a2e4cb11e8ae1,1583465980374; JSESSIONID=aaaizOhDuuIQAbNQkLXfx; SESSION_FROM_COOKIE=unknown; DICT_UGC=be3af0da19b5c5e6aa4e17bd8d90b28a|; JSESSIONID=abcij4oWdK2hB_yRTSXfx; ___rl__test__cookies=1586762011753',
             'Referer': 'http://fanyi.youdao.com/',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.204 Safari/537.36'
         }
-        return headers
 
     def fanyi(self):
-        response = requests.post(self.url, data=self.yield_form_data(), headers=self.get_headers())
-        return response.text
+        response = requests.post(self.url, data=self.yield_form_data(), headers=self.yield_headers())
+        import json
+        content = json.loads(response.text)
+        return content['translateResult'][0][0]['tgt']
 
 
 if __name__ == '__main__':
-    youdao = Youdao('南即离马')
-    print(youdao.fanyi())
+    while (True):
+        i = input("please input :")
+        youdao = Youdao(i)
+        print("fanyi result :", youdao.fanyi())
